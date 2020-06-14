021c48f6125624d1506638d4ace39636cf80a3a6
Aleksandr <oblivionforeva1@ya.ru>
2020-06-14 16:46:26 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+console.log("Hi");
+alert("11");
+
+const obj = {
+    name:"John"
+};
+const result = confirm("h");
+console.log(result);
+
+//const answer = +prompt("do u 18 ?", "");
+//console.log(typeof(answer));
+const answers = ["","" , "",];
+console.log(typeof(null));
+const category = 'toys';
+let hi = '111'; 
+console.log(`https://url.com/${hi}`);
+console.log(`\``);
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(typeof({}));
